e21a64d696f84739f0687a962f64eb152413f8e4
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2026-02-25 16:26:17 +0700

    minor fix

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -183,7 +183,7 @@ export const HeroSection = () => {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ delay: 1.2 }}
-                    className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-gray-400 cursor-pointer"
+                    className="absolute bottom-8 left-1/2 -translate-x-1/2 hidden md:flex flex-col items-center gap-2 text-gray-400 cursor-pointer"
                     onClick={scrollToProjects}
                 >
                     <motion.div
